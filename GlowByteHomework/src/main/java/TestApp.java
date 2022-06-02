9ceaafd23b94940ce0c32f5c01db5e374622cbfe
GrishinVse <vsevolod.grishin01@gmail.com>
2022-06-02 23:00:04 +0300

    Commit #1 GlowByte
    GlowByte homework

---
+++ GlowByteHomework/src/main/java/TestApp.java
@@ -0,0 +1,127 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestApp {
+    static final private String pathToSave = "C:/Users/vsevo/IdeaProjects/result.txt";
+    static final String JDBC_DRIVER = "org.h2.Driver";
+    static final String DB_URL = "jdbc:h2:file:D:/H2_database/glowbyte";
+
+    static final String USER = "vsevo";
+    static final String PASS = "1234";
+
+    public static ArrayList<ArrayList<String>> selectFromTable(String tableName) {
+        Connection conn = null;
+        Statement stmt = null;
+        try {
+            Class.forName(JDBC_DRIVER);
+
+            conn = DriverManager.getConnection(DB_URL,USER,PASS);
+
+            stmt = conn.createStatement();
+
+            if (tableName.strip().toLowerCase().equals("table_list")) {
+                String sql = "SELECT TABLE_NAME, PK FROM TABLE_LIST";
+                ResultSet rs = stmt.executeQuery(sql);
+
+                ArrayList<ArrayList<String>> table_list = new ArrayList<ArrayList<String>>();
+
+                while(rs.next()) {
+                    List<String> line = new ArrayList<String>();
+
+                    String TABLE_NAME  = rs.getString("TABLE_NAME").strip().toLowerCase();
+                    String PK = rs.getString("PK").strip().toLowerCase();
+
+                    String[] words = PK.split(",");
+                    if (words.length > 1) {
+                        for (String word: words) {
+                            line.add(TABLE_NAME);
+                            line.add(word.strip().toLowerCase());
+                            table_list.add((ArrayList<String>) line);
+                            line = new ArrayList<String>();
+                        }
+                    } else {
+                        line.add(TABLE_NAME);
+                        line.add(PK);
+                        table_list.add((ArrayList<String>) line);
+                    }
+                }
+
+                rs.close();
+                return table_list;
+            } else if (tableName.strip().toLowerCase().equals("table_cols")) {
+                String sql = "SELECT TABLE_NAME, COLUMN_NAME, COLUMN_TYPE FROM TABLE_COLS";
+                ResultSet rs = stmt.executeQuery(sql);
+
+                ArrayList<ArrayList<String>> table_cols = new ArrayList<ArrayList<String>>();
+
+                while(rs.next()) {
+                    List<String> line = new ArrayList<String>();
+
+                    String TABLE_NAME  = rs.getString("TABLE_NAME").strip().toLowerCase();
+                    String COLUMN_NAME = rs.getString("COLUMN_NAME").strip().toLowerCase();
+                    String COLUMN_TYPE = rs.getString("COLUMN_TYPE").strip().toLowerCase();
+
+                    line.add(TABLE_NAME);
+                    line.add(COLUMN_NAME);
+                    line.add(COLUMN_TYPE);
+
+                    table_cols.add((ArrayList<String>) line);
+                }
+
+                rs.close();
+                return table_cols;
+            }
+        } catch(SQLException se) {
+            se.printStackTrace();
+        } catch(Exception e) {
+            e.printStackTrace();
+        } finally {
+            try{
+                if(stmt!=null) stmt.close();
+            } catch(SQLException se2) {
+            }
+            try {
+                if(conn!=null) conn.close();
+            } catch(SQLException se){
+                se.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    public static void saveRes(ArrayList<ArrayList<String>> table1, ArrayList<ArrayList<String>> table2) {
+        String result = "";
+        for (ArrayList<String> el_1 : table1) {
+            for (ArrayList<String> el_2 : table2) {
+                if (el_1.equals(el_2.subList(0, 2))) {
+                    ArrayList<String> line = el_1;
+                    line.add(el_2.get(2));
+                    result += String.join(", ", line) + "\n";
+                    break;
+                }
+            }
+        }
+        System.out.println(result);
+
+        try {
+            FileWriter myWriter = new FileWriter(pathToSave);
+            myWriter.write(result);
+            myWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        ArrayList<ArrayList<String>> res1 = selectFromTable("TABLE_LIST");
+        System.out.println(res1);
+
+        ArrayList<ArrayList<String>> res2 = selectFromTable("TABLE_COLS");
+        System.out.println(res2);
+
+        saveRes(res1, res2);
+    }
+}
